bc8961f410671d6020da2c6c7ffa9b77817a65c5
josemiguel28 <miguel1reyes66@gmail.com>
2024-10-30 13:54:29 -0600

    UI: showing different colors depending on user state

--- views/admin/usuarios/index.php
+++ views/admin/usuarios/index.php
@@ -8,11 +8,11 @@
             <div class="flex flex-col flex-shrink-0 space-y-3 md:flex-row md:items-center justify-end md:space-y-3 md:space-x-3 ">
                 <a href="/crear-cuenta">
                     <button type="button"
-                        class="flex items-center justify-center px-4 py-2 mb-8 text-sm font-medium text-black rounded-lg">
+                            class="flex items-center justify-center px-4 py-2 mb-8 text-sm font-medium text-black rounded-lg">
                         <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                             xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path clip-rule="evenodd" fill-rule="evenodd"
-                                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"/>
                         </svg>
                         Agregar Nuevo Usuario
                     </button>
@@ -23,74 +23,91 @@
             <div class="overflow-x-auto">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                        <tr>
-                            <th scope="col" class="p-4 py-3">ID</th>
-                            <th scope="col" class="px-4 py-3">Correo</th>
-                            <th scope="col" class="px-4 py-3">Nombre</th>
-                            <th scope="col" class="px-4 py-3">Rol</th>
-                            <th scope="col" class="px-4 py-3">Telefono</th>
-                            <th scope="col" class="px-4 py-3">Fecha Creacion</th>
-                            <th scope="col" class="px-4 py-3">Estado</th>
-                            <th scope="col" class="px-4 py-3"></th>
+                    <tr>
+                        <th scope="col" class="p-4 py-3">ID</th>
+                        <th scope="col" class="px-4 py-3">Correo</th>
+                        <th scope="col" class="px-4 py-3">Nombre</th>
+                        <th scope="col" class="px-4 py-3">Rol</th>
+                        <th scope="col" class="px-4 py-3">Telefono</th>
+                        <th scope="col" class="px-4 py-3">Fecha Creacion</th>
+                        <th scope="col" class="px-4 py-3">Estado</th>
+                        <th scope="col" class="px-4 py-3"></th>
 
-                        </tr>
+                    </tr>
                     </thead>
                     <tbody>
 
-                        <?php foreach ($usuarios as $usuario) : ?>
-                            <tr class="border-b hover:bg-gray-100 dark:hover:bg-gray-700 align-center">
-                                <td class="w-4 px-4 py-3">
-                                    <?= $usuario->id; ?>
-                                </td>
-                                <th scope="row"
-                                    class="flex items-center px-2 py-2 font-medium text-gray-900 whitespace-nowrap">
-                                    <?= $usuario->correo; ?>
-                                </th>
-                                <td class="px-4 py-2">
+                    <?php foreach ($usuarios
+
+                    as $usuario) : ?>
+                    <tr class="border-b hover:bg-gray-100 dark:hover:bg-gray-700 align-center">
+                        <td class="w-4 px-4 py-3">
+                            <?= $usuario->id; ?>
+                        </td>
+                        <th scope="row"
+                            class="flex items-center px-2 py-2 font-medium text-gray-900 whitespace-nowrap">
+                            <?= $usuario->correo; ?>
+                        </th>
+                        <td class="px-4 py-2">
                                     <span class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded text-center">
                                         <?= $usuario->nombre; ?>
                                     </span>
-                                </td>
-                                <td class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded text-center">
-                                    <div class="flex items-center">
-                                        <?= $usuario->rol; ?>
-                                    </div>
-                                </td>
-                                <td class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded text-center">
-                                    <?= $usuario->telefono; ?>
-                                </td>
-                                <td class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded text-center">
-                                    <?= $usuario->token; ?>
-                                </td>
+                        </td>
+                        <td class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded text-center">
+                            <div class="flex items-center">
+                                <?= $usuario->rol; ?>
+                            </div>
+                        </td>
+                        <td class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded text-center">
+                            <?= $usuario->telefono; ?>
+                        </td>
+                        <td class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded text-center">
+                            <?= $usuario->token; ?>
+                        </td>
 
 
-                                <td class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded
-                                <?= $usuario->estado === "ACT" ? 'text-green-500' : 'text-red-500'; ?>">
+                        <td class="bg-primary-100 text-primary-800 font-medium px-2 py-0.5 rounded
+                                <?php
+                        if ($usuario->estado === "ACT") {
+                            echo 'text-green-500';
+                        } else if ($usuario->estado === "INA") {
+                            echo 'text-red-500';
+                        } else if($usuario->confirmado == 0) {
+                            echo 'text-black-500';
+                        }
 
-                                    <div class="flex items-center">
+                        ?>">
 
-                                        <?= $usuario->estado; ?>
-                                    </div>
-                                </td>
+                            <div class="flex items-center">
 
-                                <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    <div class="flex items-center justify-evenly">
-                                        <a class="bg-yellow-200 py-1 px-2 rounded-lg text-yellow-800"
-                                            href="/usuario?mode=UPD&id=<?= $usuario->id ?>">Editar</a>
+                                <?php
+                                if ($usuario->confirmado == 0) {
+                                    echo 'Usuario no confirmado';
+                                } else {
+                                    echo $usuario->estado === "INA" ? 'INA' : "ACT";
+                                }
+                                ?>
+                            </div>
+                        </td>
+
+                        <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <div class="flex items-center justify-evenly">
+                                <a class="bg-yellow-200 py-1 px-2 rounded-lg text-yellow-800"
+                                   href="/usuario?mode=UPD&id=<?= $usuario->id ?>">Editar</a>
 
 
-                                        <a class="bg-green-200 py-1 px-2 rounded-lg text-green-800"
-                                            href="index.php?page=Admin_Formulario&mode=DSP&id={{servicioId}}">Ver</a>
+                                <a class="bg-green-200 py-1 px-2 rounded-lg text-green-800"
+                                   href="index.php?page=Admin_Formulario&mode=DSP&id={{servicioId}}">Ver</a>
 
 
-                                        <a class="bg-red-200 py-1 px-2 rounded-lg text-red-800"
-                                            href="/usuario?mode=DEL&id=<?= $usuario->id ?>">Eliminar</a>
+                                <a class="bg-red-200 py-1 px-2 rounded-lg text-red-800"
+                                   href="/usuario?mode=DEL&id=<?= $usuario->id ?>">Eliminar</a>
 
 
-                                    </div>
-                                </td>
+                            </div>
+                        </td>
                     </tbody>
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
                 </table>
             </div>
 
