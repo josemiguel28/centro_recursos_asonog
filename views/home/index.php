838502e247e14f313d07f7420a35ca9454875351
josemiguel28 <miguel1reyes66@gmail.com>
2024-11-11 15:23:49 -0600

    .

--- views/home/index.php
+++ views/home/index.php
@@ -357,9 +357,12 @@
         <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6 contenedor-libros">
 
             <?php foreach ($libros as $libro) : ?>
-                <div class="w-full mb-7 max-w-sm bg-white flex flex-col justify-between sm:max-w-xs" data-aos="flip-left" data-aos-duration="1500">
-                    <a href="/libros/<?= $libro->archivo_url ?>" class="flex justify-center p-4 sm:p-8" target="_blank" style="background-color: #FCFCF7;">
-                        <img class="rounded-xl" loading="lazy" src="imagenes/<?php echo $libro->imagen ?>" alt="hero img">
+                <div class="w-full mb-7 max-w-sm bg-white flex flex-col justify-between sm:max-w-xs"
+                     data-aos="flip-left" data-aos-duration="1500">
+                    <a href="/libros/<?= $libro->archivo_url ?>" class="flex justify-center p-4 sm:p-8" target="_blank"
+                       style="background-color: #FCFCF7;">
+                        <img class="rounded-xl" loading="lazy" src="imagenes/<?php echo $libro->imagen ?>"
+                             alt="hero img">
                     </a>
                     <div class="flex flex-col justify-between h-full px-3 sm:px-5 pb-3 sm:pb-5">
                         <a href="/libros/<?= $libro->archivo_url ?>" target="_blank">
@@ -369,7 +372,7 @@
                             <div class="flex items-center space-x-1 rtl:space-x-reverse">
                                 <div class="flex items-center space-x-1 rtl:space-x-reverse">
                                     <div class="flex items-start space-x-1 rtl:space-x-reverse px-2 py-1 rounded-[1rem] bg-secondary-500">
-                                        <p class="text-xs sm:text-sm text-white max-w-[150px] truncate"><?= $libro->id_categoria ?></p>
+                                        <p class="text-xs sm:text-sm text-white max-w-[150px] truncate"><?= $libro->id_categoria //nombre de la categoria  ?></p>
                                     </div>
                                 </div>
                             </div>
